0b8cfc0468e0d79d8aa9cc4015c61e5cd3a77649
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-19 15:53:07 +0000

    Add details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const Cube = require('../models/Cube');
+const db = require('../db.json');
 
 exports.getCreateCube = (req, res) => {
     res.render('create');
@@ -11,4 +12,18 @@ exports.postCreateCube = (req, res) => {
     Cube.save(cube);
 
     res.redirect('/');
+};
+
+exports.getDetails = (req, res) => {
+    let cubeId = Number(req.params.cubeId);
+  
+    if(!cubeId){
+        return res.redirect('404');
+    }
+    let cube = db.cubes.find(c => c.id === cubeId);
+
+    if(!cube){
+        return res.redirect('404');
+    }
+    res.render('details', { cube });
 };
