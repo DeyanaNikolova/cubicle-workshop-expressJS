1bc1f9e0dc095250cc1b0fd9ae707352c16f8fe9
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-19 12:11:55 +0000

    Add views

--- src/index.js
+++ src/index.js
@@ -9,8 +9,20 @@ setupViewEngine(app);
 app.use(express.static('./src/public'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('index');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+
+
+
+
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
 
