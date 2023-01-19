e554f01d0560d814a312771c4113520df3610cbc
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-19 14:55:40 +0000

    Add create template

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -5,8 +5,9 @@ exports.getCreateCube = (req, res) => {
 };
 
 exports.postCreateCube = (req, res) => {
+    const { name, description, imageUrl, difficultyLevel} = req.body;
+    let cube = new Cube(name, description, imageUrl, difficultyLevel);
     
-    let cube = new Cube(req.body);
     Cube.save(cube);
 
     res.redirect('/');
