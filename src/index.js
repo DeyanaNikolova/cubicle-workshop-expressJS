f29f133c41d3ae0f47d18ee5aaa046c312100ba5
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-28 11:26:38 +0000

    Add cube model

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(routes);
 
 initDatabase()
 .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
-.catch((err)=> console.console.error(err));
+.catch((err)=> console.console.error(err.message));
 
 
 
