2466b5d2d0fb68d93576ca67086a3b05e7632e6f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-28 13:53:10 +0000

    Add create accessory DB

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
-
+const { restart } = require('nodemon');
+const Accessory = require('../models/Accessory');
 
 // URL : '/accessory/create'
 router.get('/create', (req, res) => {
     res.render('accessory/create');
+});
+
+router.post('/create', async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+  const accessoryDb =  await Accessory.create({ name, description, imageUrl });
+
+  console.log(accessoryDb._id);
+
+  res.redirect('/');
 });
 
 module.exports = router;
