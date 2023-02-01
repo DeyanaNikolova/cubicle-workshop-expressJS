87d373d01c6f602e538e8639219fb7388c30c6bb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-02-01 16:55:01 +0000

    Add authServices

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,11 @@
 const User = require('../models/User');
 
+
+exports.getUserByName = (username) => User.findOne({ username });
+
+exports.register = (username, password) => User.findOne({ username,  });
+
+
 exports.login = (username, password) =>{
 
 }
